fa1de66976a8706b73f8d1603256a86198d8fcc9
liuliang <liuliang@fulu.com>
2020-03-08 18:50:54 +0800

    1

--- Gourd.Application/ViewModel/Default/LoginRequest.cs
+++ Gourd.Application/ViewModel/Default/LoginRequest.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Gourd.Application.ViewModel.Default
 {
     public class LoginRequest
     {
+        [Required(ErrorMessage = "账号不能为空")]
         public string account { get; set; }
 
+        [Required(ErrorMessage = "密码不能为空")]
         public string pwd { get; set; }
     }
 }
